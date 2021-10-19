e311ae226cd9cfaeff4593c05a3a4f6c3ec8d2f3
Sweeney, Kevin <kevin.sweeney@novartis.com>
2021-10-19 15:46:36 +0100

    A poor "fix" to the issue of the left hand side panel not showing again on selection

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ class Manager(QtCore.QObject):
         if self.view is not None:
             self.view.show()
 
+    @QtCore.pyqtSlot()
+    def reinit_gui(self):
+        self._view = ExplorerWindow()
+
 
 qt_manager = Manager()
 
@@ -33,7 +37,7 @@ app = dash.Dash()
 app.layout = html.Div(
     children=[
         html.H1(children="Hello Dash"),
-        html.Button("show pop up", id="button"),
+        html.Button("show pop up", id="startQT_btn"),
         html.H2(children="", id="result"),
     ]
 )
@@ -41,30 +45,44 @@ app.layout = html.Div(
 
 @app.callback(
     Output(component_id="result", component_property="children"),
-    [Input(component_id="button", component_property="n_clicks")],
+    [Input(component_id="startQT_btn", component_property="n_clicks")],
 )
 def popUp(n_clicks):
     if not n_clicks:
         raise dash.exceptions.PreventUpdate
 
     loop = QtCore.QEventLoop()
+
+    # Connecting the ExplorerWindow closure to quiting the loop
     qt_manager.view.closed.connect(loop.quit)
+    
+    QtCore.QMetaObject.invokeMethod(
+        qt_manager, "reinit_gui", QtCore.Qt.QueuedConnection
+    )
+
     QtCore.QMetaObject.invokeMethod(
         qt_manager, "show_popup", QtCore.Qt.QueuedConnection
     )
     loop.exec_()
+
 
     return "You saw a pop-up"
 
 
 def main():
     qt_app = QtWidgets.QApplication.instance()
+
     if qt_app is None:
         qt_app = QtWidgets.QApplication([os.getcwd()])
+    
+    # Do not want to quit the app when the window is closed
     qt_app.setQuitOnLastWindowClosed(False)
     qt_manager.init_gui()
+
     threading.Thread(
-        target=app.run_server, kwargs=dict(debug=False), daemon=True,
+        target=app.run_server, 
+        kwargs=dict(debug=False), 
+        daemon=True,
     ).start()
 
     return qt_app.exec_()
